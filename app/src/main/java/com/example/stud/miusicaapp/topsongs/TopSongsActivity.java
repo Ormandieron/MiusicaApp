c81e0ab414feadd9625e03b677346afadd84fa47
W57006 <seba900000@interia.pl>
2018-04-19 17:26:56 +0200

    nowa aktywnosc

---
+++ app/src/main/java/com/example/stud/miusicaapp/topsongs/TopSongsActivity.java
@@ -0,0 +1,15 @@
+package com.example.stud.miusicaapp.topsongs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.stud.miusicaapp.R;
+
+public class TopSongsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_top_songs);
+    }
+}
