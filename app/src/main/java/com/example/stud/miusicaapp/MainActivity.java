c20b4364d08324e7b40e7179dcd249f9194295a3
W57006 <seba900000@interia.pl>
2018-04-19 17:39:00 +0200

    przycisk wtecz

--- app/src/main/java/com/example/stud/miusicaapp/MainActivity.java
+++ app/src/main/java/com/example/stud/miusicaapp/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.stud.miusicaapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import com.example.stud.miusicaapp.topsongs.TopSongsActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,7 +20,13 @@ public class MainActivity extends AppCompatActivity {
         bTopSongs.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "TODO", Toast.LENGTH_SHORT).show();
+                @Override
+                        public void onClick (View view){
+
+                    Intent intent = new Intent(MainActivity.this, TopSongsActivity.class );
+                    startActivity(intent);
+
+                }
             }
         });
     }
--- app/src/main/java/com/example/stud/miusicaapp/topsongs/TopSongsActivity.java
+++ app/src/main/java/com/example/stud/miusicaapp/topsongs/TopSongsActivity.java
@@ -8,8 +8,23 @@ import com.example.stud.miusicaapp.R;
 public class TopSongsActivity extends AppCompatActivity {
 
     @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return super.onSupportNavigateUp();
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top_songs);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        
+
+        @Override
+        public boolean onSupportNavigateUp() {
+            onBackPressed();
+            return true ;
+        }
     }
 }
