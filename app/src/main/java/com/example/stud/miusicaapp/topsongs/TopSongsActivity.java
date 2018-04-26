e760a9301f12eb733076f1c97052ec94166205d6
W57006 <seba900000@interia.pl>
2018-04-26 18:13:01 +0200

    Dokonczenie listy

--- app/src/main/java/com/example/stud/miusicaapp/topsongs/TopSongsActivity.java
+++ app/src/main/java/com/example/stud/miusicaapp/topsongs/TopSongsActivity.java
@@ -3,6 +3,7 @@ package com.example.stud.miusicaapp.topsongs;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.widget.Toast;
@@ -10,7 +11,11 @@ import android.widget.Toast;
 import com.example.stud.miusicaapp.R;
 import com.example.stud.miusicaapp.api.ApiServices;
 import com.example.stud.miusicaapp.api.TrendingList;
+import com.example.stud.miusicaapp.api.TrendingSingle;
 import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -19,7 +24,7 @@ import retrofit2.Response;
 public class TopSongsActivity extends AppCompatActivity {
 
     RecyclerView rvList;
-
+    List<TrendingSingle> trendingSingles = new ArrayList<>( 0 );
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +34,14 @@ public class TopSongsActivity extends AppCompatActivity {
 
         rvList = findViewById(R.id.rvList);
 
+        TopSongsAdapter topSongsAdapter = new TopSongsAdapter( trendingSingles );
+        rvList .setAdapter(topSongsAdapter);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager( this );
+        linearLayoutManager.setReverseLayout(true);
+        linearLayoutManager.setStackFromEnd(true);
+        rvList .setLayoutManager(linearLayoutManager);
+
         Call<TrendingList> trendingListCall = ApiServices.getService().getTrendingList( "us" ,
                 "itunes" , "singles" );
         trendingListCall.enqueue( new Callback<TrendingList>() {
@@ -37,11 +50,11 @@ public class TopSongsActivity extends AppCompatActivity {
                     Response<TrendingList> response) {
                 TrendingList trendingList = response.body();
 
-                Toast.makeText(TopSongsActivity.this, "elemetow: " + trendingList.trending.size(),
-                        Toast.LENGTH_SHORT).show();
-
-
-                Log.d ( "TAG" , new Gson().toJson(trendingList));
+                if (trendingList != null ) {
+                    TopSongsActivity. this . trendingSingles .clear();
+                    TopSongsActivity. this . trendingSingles .addAll(trendingList. trending );
+                    TopSongsActivity. this . rvList .getAdapter().notifyDataSetChanged();
+                }
             }
             @Override
             public void onFailure(@NonNull Call<TrendingList> call, Throwable t) {
